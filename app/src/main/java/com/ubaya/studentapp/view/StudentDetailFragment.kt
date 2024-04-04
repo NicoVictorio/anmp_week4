a0e1b1286aef139298cfbfc3467205d3f66d91e6
tygarean <nico.victorio@gmail.com>
2024-04-04 15:19:32 +0700

    week7

--- app/src/main/java/com/ubaya/studentapp/view/StudentDetailFragment.kt
+++ app/src/main/java/com/ubaya/studentapp/view/StudentDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.ubaya.studentapp.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,6 +15,10 @@ import com.ubaya.studentapp.databinding.FragmentStudentListBinding
 import com.ubaya.studentapp.model.Student
 import com.ubaya.studentapp.viewmodel.DetailViewModel
 import com.ubaya.studentapp.viewmodel.ListViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class StudentDetailFragment : Fragment() {
     private lateinit var binding:FragmentStudentDetailBinding
@@ -33,13 +38,29 @@ class StudentDetailFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
         viewModel.fetch()
 
-        viewModel.studentLD.observe(viewLifecycleOwner, Observer { student -> updateUI(student) })
+        observeViewModel()
     }
 
-    private fun updateUI(student: Student) {
-        binding.txtId.setText(student.id)
-        binding.txtNama.setText(student.name)
-        binding.txtBod.setText(student.bod)
-        binding.txtPhone.setText(student.phone)
+    fun observeViewModel() {
+        viewModel.studentLD.observe(viewLifecycleOwner, Observer {
+            var student = it
+
+            binding.txtId.setText(student.id)
+            binding.txtNama.setText(student.name)
+            binding.txtBod.setText(student.bod)
+            binding.txtPhone.setText(student.phone)
+
+            binding.btnUpdate?.setOnClickListener {
+                Observable.timer(5, TimeUnit.SECONDS)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe {
+                        Log.d("Messages", "five seconds")
+                        MainActivity.showNotification(student.name.toString(),
+                            "A new notification created",
+                            R.drawable.baseline_person_24)
+                    }
+            }
+        })
     }
 }
